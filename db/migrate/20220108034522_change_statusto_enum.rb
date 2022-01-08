7214ecb0f8bd252e217c4288005b9b0ae057558e
Miguel Vela <rober2miguel@gmail.com>
2022-01-08 17:17:27 +0800

    Removed untracked files that didn't work

--- db/migrate/20220108034522_change_statusto_enum.rb
+++
@@ -1,5 +0,0 @@
-class ChangeStatustoEnum < ActiveRecord::Migration[6.0]
-  def change
-      change_column :participants, :status, :integer, using: 'status::integer'
-  end
-end
--- db/migrate/20220108074459_add_column_host_id_to_radars.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnHostIdToRadars < ActiveRecord::Migration[6.0]
-  def change
-    add_column :radars, :host_id, :integer
-  end
-end
