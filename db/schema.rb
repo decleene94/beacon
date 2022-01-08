8456084d166bfdcd56a7a867d498b58a76e3f0ad
soonteckling <soonteckl@outlook.com>
2022-01-08 15:21:02 +0800

    remove submit button, press 'enter' to send message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_131823) do
+ActiveRecord::Schema.define(version: 2022_01_08_071541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2021_12_27_131823) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.text "content"
+    t.string "content"
     t.bigint "radar_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
