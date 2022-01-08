9792eda9b8e007156af8aed235accff4ed34983e
John DeCleene <decleene94@gmail.com>
2022-01-08 12:03:11 +0800

    twilio functionality - beta

---
+++ app/services/twilio_client.rb
@@ -0,0 +1,22 @@
+class TwilioClient
+  attr_reader :client
+
+  def initialize
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
+  private
+
+    def account_sid
+      Rails.application.credentials.twilio[:account_sid]
+    end
+
+    def auth_token
+      Rails.application.credentials.twilio[:auth_token]
+    end
+
+    def phone_number
+      Rails.application.credentials.twilio[:phone_number]
+    end
+
+end
