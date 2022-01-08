cd3e7ca5d955c49835f16937b10623a459beb9d3
John DeCleene <decleene94@gmail.com>
2022-01-08 17:08:36 +0800

    ran migration with updated messaging

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2022_01_08_075654) do
 
+ActiveRecord::Schema.define(version: 2022_01_08_075654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
