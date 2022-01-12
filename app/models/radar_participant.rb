3866e34bc3738ba232b094bd2cfeea002c632563
Miguel Vela <rober2miguel@gmail.com>
2022-01-12 19:58:39 +0800

    User validation added inside the participants model

--- app/models/radar_participant.rb
+++ app/models/radar_participant.rb
@@ -3,4 +3,6 @@ class RadarParticipant < ApplicationRecord
   # belongs_to :radar
   belongs_to :radar, class_name: "Radar"
   belongs_to :user, class_name: "User"
+  validates :user, uniqueness: true
+
 end
