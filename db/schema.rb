2a53392c27447de6809308da83efcfa1a2b60d88
soonteckling <soonteckl@outlook.com>
2022-01-15 15:50:20 +0800

    commit change on db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_01_15_064704) do
 
   # These are extensions that must be enabled in order to support this database
@@ -77,7 +76,6 @@ ActiveRecord::Schema.define(version: 2022_01_15_064704) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "creator_id"
     t.boolean "private", default: false
-    t.datetime "time"
     t.float "longitude"
     t.float "latitude"
     t.datetime "time"
