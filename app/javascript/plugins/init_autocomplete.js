48a882d7622de4d4151958a245ddd0bfd4e0c6bf
Miguel Vela <rober2miguel@gmail.com>
2022-01-18 20:29:35 +0800

    Refactoring my code

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -10,15 +10,10 @@
 // export { initAutocomplete };
 
 function initAutocomplete() {
-  // Create the autocomplete object, restricting the search to geographical
-  // location types.
   autocomplete = new google.maps.places.Autocomplete(
             (document.getElementById('beacon_address')),
     { types: ['geocode'] });
-  // When the user selects an address from the dropdown, populate the address
-  // fields in the form.
   autocomplete.addListener('place_changed', fillInAddress);
 }
-
 
 export { initAutocomplete };
