65240ea5fc844fc34c522861231405c6c9748007
Miguel Vela <rober2miguel@gmail.com>
2022-01-18 10:46:44 +0800

    Tested refactored code in localhost. Ready for heroku testing

--- app/controllers/radars_controller.rb
+++ app/controllers/radars_controller.rb
@@ -2,7 +2,7 @@ class RadarsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    sorted_radars = Radar.near(current_user).presentable(current_user)
+    @sorted_radars = Radar.near((current_user)).presentable(current_user)
     presentable_radars = Radar.presentable(current_user)
     @user = current_user
 
@@ -10,7 +10,7 @@ class RadarsController < ApplicationController
       # to do - test with more beacons that are scheduled for the current day
       @radars = presentable_radars.sort_by(&:time)
     elsif params[:order] == 'distance'
-      @radars = sorted_radars
+      @radars = @sorted_radars
       # @radars = current_user.nearbys(&:distance)
     else
       @radars = presentable_radars
