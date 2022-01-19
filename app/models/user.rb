2ae7517b432ae4dc001667bd5fc84b114c1e05d0
soonteckling <soonteckl@outlook.com>
2022-01-19 20:43:00 +0800

    add depenedent destroy to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :messages
+  has_many :messages, dependent: :destroy
   has_one_attached :photo
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
   has_many :followees, through: :followed_users
