2e9dcba3e6a9e5e485feb7da7be0b21c2ec33ff3
Miguel Vela <rober2miguel@gmail.com>
2022-01-22 08:34:40 +0800

    Refactored code to fix some css issues and js optimisation

--- app/javascript/packs/controllers/location_controller.js
+++ app/javascript/packs/controllers/location_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   // your logic (controller actions)
-  static targets = ['button', 'latitude', 'longitude', 'field']
+  static targets = ['button', 'latitude', 'longitude']
 
   connect() {
     // console.log(this.buttonTarget)
@@ -15,13 +15,14 @@ export default class extends Controller {
     // console.log(position.coords.longitude);
     this.latitudeTarget.value = position.coords.latitude;
     this.longitudeTarget.value = position.coords.longitude;
-    this.buttonTarget.innerText = 'You have picked your current location!';
-    const address = document.getElementById('beacon_address');
-    console.log(address());
+    this.buttonTarget.innerText = 'You have selected your current location!';
+    const address = document.querySelector('#beacon_address');
+    address.style.display = 'none';
+    const label = document.querySelector("label[for=beacon_address]")
+    label.style.display = 'none';
   };
 
   errorCallback(error) {
-    // fieldTarget.innerText = 'Please enter your address!!!'
     console.error(error);
   };
 
--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -10,11 +10,26 @@
 // export { initAutocomplete };
 
 function initAutocomplete() {
-  autocomplete = new google.maps.places.Autocomplete(
-            (document.getElementById('beacon_address')),
-    { types: ['geocode'] });
-  autocomplete.addListener('place_changed', fillInAddress);
+
+  let defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(103.8198, 1.3521),
+    );
+
+    let input = document.getElementById('beacon_address');
+    let options = {
+      bounds: defaultBounds,
+      types: ['geocode'], //['establishment']
+      country: ['singapore']
+    };
+
+    const autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    autocomplete.setComponentRestrictions({
+      country: ["sg"],
+    });
+
+    autocomplete.setBounds(defaultBounds);
+    autocomplete.addListener('place_changed', fillInAddress);
+
 }
-
 export { initAutocomplete };
-
